ac2c0ff57f7d89d1c47b86e812ea02c27c735b95
Jonathan Cheng <yuch1617@colorado.edu>
2026-02-04 01:51:08 +0000

    Part 1: create VM with startup script and firewall

--- part1/part1.py
+++ part1/part1.py
@@ -1,23 +1,115 @@
 #!/usr/bin/env python3
 
-import argparse
-import os
 import time
-from pprint import pprint
-
 import googleapiclient.discovery
 import google.auth
 
 credentials, project = google.auth.default()
 service = googleapiclient.discovery.build('compute', 'v1', credentials=credentials)
 
+ZONE = 'us-west1-b'
+INSTANCE_NAME = 'flask-vm'
+MACHINE_TYPE = 'e2-medium'   #  f1-micro
+
+STARTUP_SCRIPT = """#!/bin/bash
+set -e
+mkdir -p /opt/app
+cd /opt/app
+apt-get update
+apt-get install -y python3 python3-pip git
+git clone https://github.com/cu-csci-4253-datacenter/flask-tutorial
+cd flask-tutorial
+python3 setup.py install
+pip3 install -e .
+export FLASK_APP=flaskr
+flask init-db
+nohup flask run -h 0.0.0.0 -p 5000 &
+"""
+
 #
 # Stub code - just lists all instances
 #
 def list_instances(compute, project, zone):
     result = compute.instances().list(project=project, zone=zone).execute()
-    return result['items'] if 'items' in result else None
+    return result['items'] if 'items' in result else []
+
+def wait_for_operation(op, zone=None):
+    while True:
+        if zone:
+            result = service.zoneOperations().get(
+                project=project, zone=zone, operation=op['name']).execute()
+        else:
+            result = service.globalOperations().get(
+                project=project, operation=op['name']).execute()
+        if result['status'] == 'DONE':
+            return
+        time.sleep(2)
+
+def create_firewall_rule():
+    firewall_body = {
+        'name': 'allow-5000',
+        'network': 'global/networks/default',
+        'direction': 'INGRESS',
+        'sourceRanges': ['0.0.0.0/0'],
+        'targetTags': ['allow-5000'],
+        'allowed': [{'IPProtocol': 'tcp', 'ports': ['5000']}],
+    }
+    op = service.firewalls().insert(
+        project=project, body=firewall_body).execute()
+    wait_for_operation(op)
+
+def create_instance():
+    image = service.images().getFromFamily(
+        project='ubuntu-os-cloud',
+        family='ubuntu-2204-lts').execute()
+
+    config = {
+        'name': INSTANCE_NAME,
+        'machineType': f'zones/{ZONE}/machineTypes/{MACHINE_TYPE}',
+        'tags': {'items': ['allow-5000']},
+        'disks': [{
+            'boot': True,
+            'autoDelete': True,
+            'initializeParams': {
+                'sourceImage': image['selfLink'],
+                'diskSizeGb': '10',
+            }
+        }],
+        'networkInterfaces': [{
+            'network': 'global/networks/default',
+            'accessConfigs': [{
+                'name': 'External NAT',
+                'type': 'ONE_TO_ONE_NAT'
+            }]
+        }],
+        'metadata': {
+            'items': [{
+                'key': 'startup-script',
+                'value': STARTUP_SCRIPT
+            }]
+        }
+    }
+
+    op = service.instances().insert(
+        project=project, zone=ZONE, body=config).execute()
+    wait_for_operation(op, ZONE)
+
+def get_external_ip():
+    inst = service.instances().get(
+        project=project, zone=ZONE, instance=INSTANCE_NAME).execute()
+    for nic in inst['networkInterfaces']:
+        for ac in nic.get('accessConfigs', []):
+            if 'natIP' in ac:
+                return ac['natIP']
+    return None
 
 print("Your running instances are:")
-for instance in list_instances(service, project, 'us-west1-b'):
+for instance in list_instances(service, project, ZONE):
     print(instance['name'])
+
+create_firewall_rule()
+create_instance()
+
+ip = get_external_ip()
+print("\nVisit:")
+print(f"http://{ip}:5000")
