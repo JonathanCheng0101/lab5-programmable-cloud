8b03217d445d159872501852f5df533ce16079ea
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-02-03 22:44:49 -0700

    Update part1.py

--- part1/part1.py
+++ part1/part1.py
@@ -7,9 +7,9 @@ import google.auth
 credentials, project = google.auth.default()
 service = googleapiclient.discovery.build('compute', 'v1', credentials=credentials)
 
-ZONE = 'us-west1-a'
+ZONE = 'us-west1-b'
 INSTANCE_NAME = 'flask-vm'
-MACHINE_TYPE = 'e2-standard-2'   #  f1-micro
+MACHINE_TYPE = 'f1-micro'   #  e2-standard-2
 
 STARTUP_SCRIPT = """#!/bin/bash
 set -e
